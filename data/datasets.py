02b0471e45d42b37f681edc8cf1180c71baf5631
harim <hrjeong04@kaist.ac.kr>
2024-05-19 07:30:47 +0000

    experiments1

--- data/datasets.py
+++ data/datasets.py
@@ -198,35 +198,30 @@ class FromNumpyDataset(Dataset):
 
 class Stock(Dataset):
     def __init__(self,
-                 symbol : str = "AAPL", 
-                 sdate : str = "2000", 
-                 edate : str = "2024",
+                 symbol : str = "AAPL, MSFT, NVDA, AMZN, COST", 
+                 sdate : str = "20000101", 
+                 edate : str = "20131231",
                  window : int = 24,
-                 save_ground_truth=True, 
-                 normalize=True,
-                 period='train'
-                 ):
+                 save_ground_truth : bool =True, 
+                 period : str ='train'):
         super().__init__()
         raw_df = fdr.DataReader(symbol, sdate, edate)
-        self.data = self.generate_stock_sample(raw_df, window)
+        self.data, self.mean, self.std = self._generate_stock_sequence(raw_df, window)
         self.window = window
         self.feature_dim = self.data.shape[-1]
-        self.dir = './output'
+        self.dir = './dataset/stock'
         os.makedirs(self.dir, exist_ok=True)
-
-        if normalize:
-            self.data, self.mean, self.std = self._mean_std_scale(self.data)
-            
         if save_ground_truth:
-            np.save(os.path.join(self.dir, f"stock_ground_truth_data_{window}_{period}.npy"), self.data)
-            np.save(os.path.join(self.dir, f"stock_ground_truth_mean_{window}_{period}.npy"), self.mean)
-            np.save(os.path.join(self.dir, f"stock_ground_truth_std_{window}_{period}.npy"), self.std)
-
-    def generate_stock_sample(self, df, window):
+            np.save(os.path.join(self.dir, f"origin_norm_{window}_{period}.npy"), self.data)
+            np.save(os.path.join(self.dir, f"origin_mean_{window}_{period}.npy"), self.mean)
+            np.save(os.path.join(self.dir, f"origin_std_{window}_{period}.npy"), self.std)
+    
+    def _generate_stock_sequence(self, df, window):
         raw_data = torch.from_numpy(df.to_numpy()).float()
         data = self._extract_sliding_windows(raw_data, window)
-        
-        return data
+        data, mean, std = self._mean_std_scale(data)
+
+        return data, mean, std
     
     def _extract_sliding_windows(self, raw_data, window):
         sample_n = len(raw_data) - window + 1
@@ -241,36 +236,35 @@ class Stock(Dataset):
 
     def _mean_std_scale(self, data):
         epsilon = 1e-8
-        preceding_data = data[:, :-1, :]
+        past_data = data[:, :-1, :]
         target_data = data[:, -1:, :]
-        mean = torch.mean(preceding_data, dim=1, keepdim=True)
-        std = torch.std(preceding_data, dim=1, keepdim=True)
+        mean = torch.mean(past_data, dim=1, keepdim=True)
+        std = torch.std(past_data, dim=1, keepdim=True)
         
-        scaled_preceding_data = (preceding_data - mean) / (std + epsilon)
-        scaled_target_data = (target_data - mean) / (std + epsilon)
-        scaled_data = torch.cat((scaled_preceding_data, scaled_target_data), dim=1)
+        past_data_normalized = (past_data - mean) / (std + epsilon)
+        target_data_normalized = (target_data - mean) / (std + epsilon)
+        data_normalized = torch.cat((past_data_normalized, target_data_normalized), dim=1)
         
-        return scaled_data, mean, std
+        return data_normalized, mean, std
     
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return self.data[idx]
+        return self.data[idx], self.mean[idx], self.std[idx]
 
     
 class StockDifferencing(Dataset):
     def __init__(self,
-                symbol : str = "AAPL, MSFT, NVDA, AMZN, COST", 
-                sdate : str = "20000101", 
-                edate : str = "20131231",
-                window : int = 24,
-                save_ground_truth=True, 
-                period='train'
-                ):
+                 symbol : str = "AAPL, MSFT, NVDA, AMZN, COST", 
+                 sdate : str = "20000101", 
+                 edate : str = "20131231",
+                 window : int = 24,
+                 save_ground_truth : bool =True, 
+                 period : str ='train'):
         super().__init__()
         raw_df = fdr.DataReader(symbol, sdate, edate)
-        self.data_diff, self.data, self.mean, self.std = self.generate_stock_sequence(raw_df, window)
+        self.data_diff, self.data, self.mean, self.std = self._generate_stock_sequence(raw_df, window)
         self.window = window
         self.feature_dim = self.data.shape[-1]
         self.dir = './dataset/stock_diff'
@@ -282,7 +276,7 @@ class StockDifferencing(Dataset):
             np.save(os.path.join(self.dir, f"origin_mean_{window}_{period}.npy"), self.mean)
             np.save(os.path.join(self.dir, f"origin_std_{window}_{period}.npy"), self.std)
     
-    def generate_stock_sequence(self, df, window):
+    def _generate_stock_sequence(self, df, window):
         raw_data = torch.from_numpy(df.to_numpy()).float()
         data = self._extract_sliding_windows(raw_data, window)
         data, mean, std = self._mean_std_scale(data)
